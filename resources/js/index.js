b8d48257a0c5a5b107c1a4b35d84c4802afc7d31
unknown <kheansreythou17@kit.edu.kh>
2021-07-29 09:01:46 +0700

    final commit 1

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
+    // Point 1
     $("#toggle-1").click(function () {
         if ($("#content-1").is(":hidden")) {
             $('#arrow-btn-1').attr('src', './resources/images/img_circle_arrow_up.png');
@@ -7,7 +8,6 @@ $(document).ready(function () {
         }
         $("#content-1").slideToggle("slow");
     });
-
     $("#toggle-2").click(function () {
         if ($("#content-2").is(":hidden")) {
             $('#arrow-btn-2').attr('src', './resources/images/img_circle_arrow_up.png');
@@ -17,6 +17,7 @@ $(document).ready(function () {
         $("#content-2").slideToggle("slow");
     });
 
+    // Point 2
     $("#toggle-3").click(function () {
         if ($("#content-3").is(":hidden")) {
             $('#arrow-btn-3').attr('src', './resources/images/img_circle_arrow_up.png');
@@ -25,6 +26,72 @@ $(document).ready(function () {
         }
         $("#content-3").slideToggle("slow");
     });
+    $("#toggle-4").click(function () {
+        if ($("#content-4").is(":hidden")) {
+            $('#arrow-btn-4').attr('src', './resources/images/img_circle_arrow_up.png');
+        } else {
+            $('#arrow-btn-4').attr('src', './resources/images/img_circle_arrow_down.png');
+        }
+        $("#content-4").slideToggle("slow");
+    });
+    $("#toggle-5").click(function () {
+        if ($("#content-5").is(":hidden")) {
+            $('#arrow-btn-5').attr('src', './resources/images/img_circle_arrow_up.png');
+        } else {
+            $('#arrow-btn-5').attr('src', './resources/images/img_circle_arrow_down.png');
+        }
+        $("#content-5").slideToggle("slow");
+    });
+    $("#toggle-6").click(function () {
+        if ($("#content-6").is(":hidden")) {
+            $('#arrow-btn-6').attr('src', './resources/images/img_circle_arrow_up.png');
+        } else {
+            $('#arrow-btn-6').attr('src', './resources/images/img_circle_arrow_down.png');
+        }
+        $("#content-6").slideToggle("slow");
+    });
+    $("#toggle-7").click(function () {
+        if ($("#content-7").is(":hidden")) {
+            $('#arrow-btn-7').attr('src', './resources/images/img_circle_arrow_up.png');
+        } else {
+            $('#arrow-btn-7').attr('src', './resources/images/img_circle_arrow_down.png');
+        }
+        $("#content-7").slideToggle("slow");
+    });
+    $("#toggle-8").click(function () {
+        if ($("#content-8").is(":hidden")) {
+            $('#arrow-btn-8').attr('src', './resources/images/img_circle_arrow_up.png');
+        } else {
+            $('#arrow-btn-8').attr('src', './resources/images/img_circle_arrow_down.png');
+        }
+        $("#content-8").slideToggle("slow");
+    });
+    $("#toggle-9").click(function () {
+        if ($("#content-9").is(":hidden")) {
+            $('#arrow-btn-9').attr('src', './resources/images/img_circle_arrow_up.png');
+        } else {
+            $('#arrow-btn-9').attr('src', './resources/images/img_circle_arrow_down.png');
+        }
+        $("#content-9").slideToggle("slow");
+    });
+    $("#toggle-10").click(function () {
+        if ($("#content-10").is(":hidden")) {
+            $('#arrow-btn-10').attr('src', './resources/images/img_circle_arrow_up.png');
+        } else {
+            $('#arrow-btn-10').attr('src', './resources/images/img_circle_arrow_down.png');
+        }
+        $("#content-10").slideToggle("slow");
+    });
+    $("#toggle-11").click(function () {
+        if ($("#content-11").is(":hidden")) {
+            $('#arrow-btn-11').attr('src', './resources/images/img_circle_arrow_up.png');
+        } else {
+            $('#arrow-btn-11').attr('src', './resources/images/img_circle_arrow_down.png');
+        }
+        $("#content-11").slideToggle("slow");
+    });
+
+    // Point 3
     $("#toggle-12").click(function () {
         if ($("#content-12").is(":hidden")) {
             $('#arrow-btn-12').attr('src', './resources/images/img_circle_arrow_up.png');
